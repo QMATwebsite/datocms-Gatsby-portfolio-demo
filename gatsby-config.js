40b447ff99e48b2f145de7c03a2504dc47f79eb8
QMATwebsite <56088227+QMATwebsite@users.noreply.github.com>
2023-02-07 15:42:07 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-datocms`,
       options: {
